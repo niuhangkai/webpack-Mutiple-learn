ee3450495e0166fa23adcfd1ecf5cf75782116c0
牛航凯 <niuhk@rp-pet.cn>
2019-08-26 18:04:34 +0800

    add options

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,43 +2,68 @@
  * @Author: niuhangkai
  * @Date: 2019-08-24 16:26:56
  * @Last Modified by: niuhangkai
- * @Last Modified time: 2019-08-25 22:13:22
+ * @Last Modified time: 2019-08-26 17:10:59
  */
+'use strict'
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base.conf')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const portfinder = require('portfinder')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-
-
-module.exports =merge(baseConfig,{
-  // mode: 'production',
+const devWebpackConfig = merge(baseConfig, {
   mode: 'development',
   output: {
     path: path.resolve(__dirname, '../dist'),
-    // filename: "[name].[chunkhash:8].js"
-    filename: "[name].[hash:8].js"
+    filename: "[name].[hash:8].js",
+    publicPath: './'
   },
   devServer: {
-    openPage:'index.html',
+    clientLogLevel: 'warning',
     watchContentBase: true,
-		contentBase: path.join(__dirname, "../src/pages/index"),
-		publicPath:'/',
-		host: "127.0.0.1",
-		port: "8080",
-		overlay: true, // 浏览器页面上显示错误
-		// open: true, // 开启浏览器
-		// stats: "errors-only", //stats: "errors-only"表示只打印错误：
-		//服务器代理配置项
+    publicPath: '/',
+    host: "127.0.0.1",
+    port: "8080",
+    quiet: true,
+    compress: true,
+    overlay: true, // 浏览器页面上显示错误
+    // open: true, // 开启浏览器
+    // stats: "errors-only", //stats: "errors-only"表示只打印错误：
+    // notifyOnErrors: true,
+    //服务器代理配置项
     proxy: {
-        '/testing/*': {
-            target: 'https://www.baidu.com',
-            secure: true,
-            changeOrigin: true
-        }
+      '/testing/*': {
+        target: 'https://www.baidu.com',
+        secure: true,
+        changeOrigin: true
+      }
     }
-    },
+  },
+  plugins: [
+    // new ExtractTextPlugin("../dist/css/[name].css")
+    new ExtractTextPlugin({
+      filename: "css/[name].[hash:8].css",
+      allChunks: true
+    }),
+  ]
 })
-  
- 
+
+module.exports = new Promise((resolve, reject) => {
+  portfinder.basePort = devWebpackConfig.devServer.port
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err)
+    } else {
+      // 自动切换占用端口
+      devWebpackConfig.devServer.port = port
+      // 编译完成提示
+      devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
+        compilationSuccessInfo: {
+          messages: [`Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`],
+        },
+      }))
+      resolve(devWebpackConfig)
+    }
+  })
+})
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,10 @@
 /*
  * @Author: niuhangkai 
  * @Date: 2019-08-25 22:14:34 
- * @Last Modified by:   niuhangkai 
- * @Last Modified time: 2019-08-25 22:14:34 
+ * @Last Modified by: niuhangkai
+ * @Last Modified time: 2019-08-26 17:25:11
  */
 
 
 import './home.less'
 const A = 123
-import '../../main'
